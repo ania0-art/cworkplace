e87ed6a24b5f1f4fc18568094232114a9cce0622
ania0-art <75117340+ania0-art@users.noreply.github.com>
2021-06-28 15:44:48 +0800

    1

--- 学习.c
+++ 学习.c
@@ -1,5 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-28 14:57:36
+ * @LastEditTime: 2021-06-28 15:37:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \cworkplace\学习.c
+ */
 #include<stdio.h>
-void main()
-{
-    printf("hello world");
-}
--- 学生信息管理系统.c
+++ 学生信息管理系统.c
@@ -1,3 +1,11 @@
+/*
+ * @Author: 6371.393
+ * @Date: 2021-06-28 09:58:56
+ * @LastEditTime: 2021-06-28 15:41:59
+ * @LastEditors: Please set LastEditors
+ * @Description: vs code
+ * @FilePath: \cworkplace\学生信息管理系统.c
+ */
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
